fd78c2b6af8670ff138e6d9698b37708398be368
Jethet <henriette.hettinga@protonmail.com>
2023-02-12 18:08:16 +0100

    Add endpoints

--- REST/server.js
+++ REST/server.js
@@ -1,6 +1,17 @@
 var express = require("express");
-
 var app = express();
+
+// Serve static files like images, CSS and JS files: use express.static middleware
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
+
+app.get("/crazy", (req, res) => {
+    res.send("still crazy?")
+})
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
